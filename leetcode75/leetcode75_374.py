a3f432222adccf0256eeb0186f883d57364c14ed
Sudipto Sahoo <160700238+sahoosudipto@users.noreply.github.com>
2024-03-28 17:00:25 +0530

    Create leetcode75_374.py

---
+++ leetcode75/leetcode75_374.py
@@ -0,0 +1,51 @@
+'''
+374. Guess Number Higher or Lower
+
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+
+Example 1:
+Input: n = 10, pick = 6
+Output: 6
+
+Example 2:
+Input: n = 1, pick = 1
+Output: 1
+'''
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num):
+
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        low, high = 1, n
+        while low <= high:
+            # mid = (low + high ) >> 1
+            mid = (low + high ) //2
+            g = guess(mid)
+            # if guess(mid) == 0:
+            if g == 0:
+                return mid
+            # elif guess(mid) == -1:
+            elif g == -1:
+                high = mid - 1
+            else: 
+                low = mid + 1
+        return low
