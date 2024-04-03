de49de9363ef21bec813ceb80374f2014192dcf2
Sudipto Sahoo <160700238+sahoosudipto@users.noreply.github.com>
2024-04-03 21:04:04 +0530

    Create leetcode75_700.py

---
+++ leetcode75/leetcode75_700.py
@@ -0,0 +1,38 @@
+'''
+700. Search in a Binary Search Tree
+You are given the root of a binary search tree (BST) and an integer val.
+
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+
+Input: root = [4,2,7,1,3], val = 2
+Output: [2,1,3]
+
+Input: root = [4,2,7,1,3], val = 5
+Output: []
+ 
+Constraints:
+The number of nodes in the tree is in the range [1, 5000].
+1 <= Node.val <= 107
+root is a binary search tree.
+1 <= val <= 107
+'''
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if not root or root.val == val:
+            return root
+        elif root.val > val:
+            return self.searchBST(root.left, val)
+        else:
+            return self.searchBST(root.right, val)
+
